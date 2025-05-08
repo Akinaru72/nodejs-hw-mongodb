64762a0f61e06173368d338700dc76bec31d31e0
Volodymyr <akinaru3@gmail.com>
2025-05-08 16:26:45 +0300

    1

---
+++ src/utils/getEnvVar.js
@@ -0,0 +1,16 @@
+// src/utils/getEnvVar
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export function getEnvVar(name, defaultValue) {
+  const value = process.env[name];
+
+  if (value) return value;
+
+  if (defaultValue) return defaultValue;
+
+  throw new Error(`Missing: process.env['${name}'].`);
+}
+
